513aeb5aeadae3ed16b72e38fc523e5023895eda
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-10 14:50:44 +0100

    refactor: update user component to use input signals for avatar, name, and id, and implement output for user selection

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,19 +11,21 @@ export class UserComponent {
   // @Input({required: true}) avatar!: string;
   // @Input({required: true}) name!: string;
 
-  @Input({required: true}) id!: string;
+  // @Input({required: true}) id!: string;
   avatar = input.required<string>();
   name = input.required<string>();
-  @Output() select = new EventEmitter();
+
+  id = input.required<string>();
+ // @Output() select = new EventEmitter();
+  select = output<string>();
+ 
 
   imagePath = computed(() => 'assets/users/' + this.avatar());
   // get imagePath() {
   //   return 'assets/users/' + this.avatar();
   // }
 
-
- 
   onSelectUser(){
-    this.select.emit(this.id);
+    this.select.emit(this.id());
   }
 }
