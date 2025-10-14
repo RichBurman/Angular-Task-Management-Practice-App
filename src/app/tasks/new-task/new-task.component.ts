8c59143e39541a5ab874cc7ffcd66f4b23210dca
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-14 16:39:22 +0100

    feat: bind form inputs to component properties for task creation

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,23 @@
 import { Component, output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 
   cancel = output<void>();
+
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDate = '';
   onCancel() {
     this.cancel.emit();
   }
+
+  onSubmit() {}
 }
