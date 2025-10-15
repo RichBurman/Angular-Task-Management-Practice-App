2db2221cd705f0642d2b83677238b920c5293063
Richard Burman <richard.burman@harperjames.co.uk>
2025-10-15 10:47:26 +0100

    feat: enhance new task component to emit task data on submission

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,17 @@
 import { Component, output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NewTaskData } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
   imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
-
   cancel = output<void>();
+  add = output<NewTaskData>();
 
   enteredTitle = '';
   enteredSummary = '';
@@ -19,5 +20,11 @@ export class NewTaskComponent {
     this.cancel.emit();
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.add.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      date: this.enteredDate,
+    });
+  }
 }
